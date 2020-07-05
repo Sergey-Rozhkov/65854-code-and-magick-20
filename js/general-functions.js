645718fb938aa47ac927ca9fc3cc3a992fcbb2d8
viktoria.lazor@gmail.com <viktoria.lazor@gmail.com>
2020-07-05 23:28:14 +0300

    Training project: in motion

--- js/general-functions.js
+++ js/general-functions.js
@@ -1,29 +1,33 @@
 'use strict';
 
 window.generalFunctions = (function () {
-  return {
-    getMaxElement: function (arr) {
-      var maxElement = arr[0];
+  var getMaxElement = function (arr) {
+    var maxElement = arr[0];
 
-      arr.forEach(function (item) {
-        if (item > maxElement) {
-          maxElement = item;
-        }
-      });
+    arr.forEach(function (item) {
+      if (item > maxElement) {
+        maxElement = item;
+      }
+    });
 
-      return maxElement;
-    },
-
-    getRandomNumberFromRange: function (min, max) {
-      return Math.ceil(Math.random() * (max - min) + min);
-    },
-
-    getRandomIndexArray: function (arr) {
-      var min = 0;
-      var max = arr.length - 1;
-      var result = Math.round(Math.random() * (max - min) + min);
-
-      return arr[result];
-    }
+    return maxElement;
   };
+
+  var getRandomNumberFromRange = function (min, max) {
+    return Math.ceil(Math.random() * (max - min) + min);
+  };
+
+  var getRandomIndexArray = function (arr) {
+    var min = 0;
+    var max = arr.length - 1;
+    var result = Math.round(Math.random() * (max - min) + min);
+
+    return arr[result];
+  };
+
+  return {
+    getMaxElement: getMaxElement,
+    getRandomNumberFromRange: getRandomNumberFromRange,
+    getRandomIndexArray: getRandomIndexArray
+  }
 })();
